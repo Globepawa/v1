d4727bf585ef16b2ec6fc303b436843d352bff51
Globepawa <globepawaventures@gmail.com>
2023-04-22 07:50:02 +0300

    commit sd

---
+++ cont/div.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+    var div = "<div class='drop'></div>";
+
+$('.drop_cn').each(function() {
+
+for(var i= 26; div<length; i++){
+    return (div[i]);
+}
+ });
+
+}
+);
